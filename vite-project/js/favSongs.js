a12c09cfe718999197ed06e44a73d338e1e55fe8
cassidyrobles <cassidyrobles19@gmail.com>
2022-12-20 00:17:04 -0500

    :(((((

--- vite-project/js/favSongs.js
+++ vite-project/js/favSongs.js
@@ -1,9 +1,58 @@
-const menu = [
-{
-name: "mia",
-number: [2, 3, 4],
-colors: "Pink",
-},
-];
-
-export { menu };
+const favSongs = [
+    {
+        name: "Angel Of Small Death & The Codeine Scene",
+        artist: "Hozier",
+        album: "Hozier Album",
+        img: "http://www.audiofemme.com/wp-content/uploads/2015/02/hozier-album-cover.jpg",
+    },
+    {
+        name: "Cherry Wine",
+        artist: "Hozier",
+        album: "Hozier Album",
+        img: "http://www.audiofemme.com/wp-content/uploads/2015/02/hozier-album-cover.jpg",
+    },
+    {
+        name: "Someone New",
+        artist: "Hozier",
+        album: "Hozier Album",
+        img: "http://www.audiofemme.com/wp-content/uploads/2015/02/hozier-album-cover.jpg",
+    },
+    {
+        name: "Don't Blame Me",
+        artist: "Taylor Swift",
+        album: "Reputation",
+        img: "https://www.udiscovermusic.com/wp-content/uploads/2017/11/Taylor-Swift-Reputation-Album-Cover-web-730.jpg",
+    },
+    {
+        name: "New Romantics",
+        artist: "Taylor Swift",
+        album: "1989",
+        img: "http://www.aceshowbiz.com/images/news/taylor-swift-unveils-official-tracklist-for-1989-album.jpg",
+    },
+    {
+        name: "The Archer",
+        artist: "Taylor Swift",
+        album: "Lover",
+        img: "https://www.normanrecords.com/artwork/large/93/179093-taylor-swift-lover.jpg",
+    },
+    {
+        name: "Snap Out Of It",
+        artist: "Arctic Monkeys",
+        album: "AM",
+        img: "https://thefirenote.com/wp-content/uploads/2013/09/arctic-monkeys-am-cover.jpg",
+    },
+    {
+        name: "Knee Socks",
+        artist: "Arctic Monkeys",
+        album: "AM",
+        img: "https://thefirenote.com/wp-content/uploads/2013/09/arctic-monkeys-am-cover.jpg",
+    },
+    {
+        name: "Balaclava",
+        artist: "Arctic Monkeys",
+        album: "Favourite Worst Nightmare",
+        img: "http://images.genius.com/9f83ef74470b26d7c8820ff2e8e095c2.1000x1000x1.jpg",
+    },
+    ];
+    
+    export { favSongs };
