1772a68bdc18f2a03b58169d871d43f167d26cea
cassidyrobles <cassidyrobles19@gmail.com>
2022-12-20 20:38:24 -0500

    jjjh

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,12 +1,2 @@
 import { favSongs } from "./favSongs";
 import "../styles/style.css";
-document.querySelector(".btn").addEventListener("click", function () {
-  if (document.body.classList.contains("cool")) {
-    document.body.classList.add("warm");
-    document.body.classList.remove("cool");
-  } else {
-    document.body.classList.add("cool");
-    document.body.classList.remove("warm");
-  }
-});
-console.log(menu);
