23c1d881ea4121f91045c704d8687cdcc98e17fb
cassidyrobles <cassidyrobles19@gmail.com>
2022-12-21 13:16:05 -0500

    yesssss

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,3 +1,14 @@
 import { favSongs } from "./favSongs";
 import "../styles/style.css";
+import { DOMselectors } from "./DOMMMM";
 
+favSongs.forEach((el)=>{
+    DOMselectors.box.insertAdjacentHTML(
+        "beforeend",
+        `<div class="card">
+        <p>${el.name}</p>
+        <img class= "theimage" src="${el.img}">
+        <p>${el.artist}</p>
+        </div>`
+    );
+});
