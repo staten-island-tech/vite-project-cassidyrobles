8960eaf07c3d1bdff50e5982302ea9a772dbe2d4
cassidyrobles <cassidyrobles19@gmail.com>
2022-12-21 20:04:41 -0500

    hr

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,3 +1,12 @@
 import { favSongs } from "./favSongs";
 import "../styles/style.css";
 
+const DOMselectors = {
+  btn: document.querySelector(".btn"),
+  hozierbtn: document.querySelector("#hozier"),
+  tsbtn: document.querySelector("#taylor swift"),
+  ambtn: document.querySelector("#arctic monkes"),
+  displaycard: document.querySelector("display-card"),
+};
+
+
