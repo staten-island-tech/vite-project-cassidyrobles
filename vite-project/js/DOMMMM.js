ea4e9ae9871a5e25756810ff0a7b77a28e1c0477
cassidyrobles <cassidyrobles19@gmail.com>
2023-01-04 13:14:30 -0500

    qwertyuio

--- vite-project/js/DOMMMM.js
+++ vite-project/js/DOMMMM.js
@@ -12,15 +12,14 @@ const DOMselectors = {
   export { DOMselectors };
 
   DOMselectors.mode.addEventListener("click", function(){
-    if (document.body.classList.contains("light")){
-      
-      document.body.classList.add("dark");
-      document.body.classList.remove("light");
-    } else{
-      document.body.classList.add("light");
-      document.body.classList.remove("dark");
-    }
-  });
-
-
+   
+      if (document.body.classList.contains("light")){
+        
+        document.body.classList.add("dark");
+        document.body.classList.remove("light");
+      } else{
+        document.body.classList.add("light");
+        document.body.classList.remove("dark");
+        
+      }});
 
